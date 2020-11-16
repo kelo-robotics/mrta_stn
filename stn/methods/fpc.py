fbf3eb96bb069ef73f77fd75b9c8f793b38f0981
Angela Enriquez <angela.enriquez@locomotec.com>
2020-11-16 13:30:05 +0100

    fpc: Remove logging

--- stn/methods/fpc.py
+++ stn/methods/fpc.py
@@ -1,14 +1,12 @@
-import logging
-import networkx as nx
 import copy
 
+import networkx as nx
 
 """ Achieves full path consistency (fpc) by applying the Floyd Warshall algorithm to the STN"""
 
 
 def get_minimal_network(stn):
 
-    logger = logging.getLogger('stn.fpc')
     minimal_network = copy.deepcopy(stn)
 
     shortest_path_array = nx.floyd_warshall(stn)
@@ -16,5 +14,3 @@ def get_minimal_network(stn):
         # Get minimal stn by updating the edges of the stn to reflect the shortest path distances
         minimal_network.update_edges(shortest_path_array)
         return minimal_network
-    else:
-        logger.debug("The minimal network is inconsistent. STP could not be solved")
